33645e516e874e536aba9bf8d2c7a92be1a220d6
Gabriel Bridges <gabrielb71@gmail.com>
2025-07-24 10:06:16 -0400

    added ratio to spin-equilibrium model to allow R_m flexibility

--- model_funcs/ipsepc.cpp
+++ model_funcs/ipsepc.cpp
@@ -8,14 +8,15 @@ void IPspec(const RealArray& energy, const RealArray& params, int spectrum_num, 
 
     int reflection_sel = params[0]; // how to apply reflection 0 = off, 1 = on, apply at each layer, 2 = on, apply once to whole spectrum
     double p_spin = params[1]; // spin period [s]; assumes spin equilibrium
-    double fractional_area = params[2]; //fractional accretion area
-    double luminosity = params[3]*1e33; // luminosity [ergs/s]
-    double mass = params[4]*m_sol; // WD mass [grams]
-    double col_abund = params[5]; // accretion column abundance [solar abundances]
-    double cos_incl = params[6]; // cos inclination angle
-    double source_distance = params[7]*pc_to_cm; // source distnace [cm]
+    double r_m_ratio = params[2]; // ratio between corotation radius and sping period
+    double fractional_area = params[3]; //fractional accretion area
+    double luminosity = params[4]*1e33; // luminosity [ergs/s]
+    double mass = params[5]*m_sol; // WD mass [grams]
+    double col_abund = params[6]; // accretion column abundance [solar abundances]
+    double cos_incl = params[7]; // cos inclination angle
+    double source_distance = params[8]*pc_to_cm; // source distnace [cm]
 
-    double mag_radius = cbrt(4*pi*pi*grav_const*mass*p_spin*p_spin);
+    double mag_radius = r_m_ratio*cbrt(4*pi*pi*grav_const*mass*p_spin*p_spin);
     Cataclysmic_Variable intermediate_polar(mass, col_abund, luminosity, fractional_area, cos_incl, source_distance, reflection_sel, mag_radius);
     intermediate_polar.Shock_Height_Shooting(1000);
     intermediate_polar.MCVspec_Spectrum(energy, spectrum_num, flux, init_string);
