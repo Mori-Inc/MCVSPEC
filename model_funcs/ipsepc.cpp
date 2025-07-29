385e866a0166b28d87f57c308b71eb6b4747cf47
Gabriel Bridges <gabrielb71@gmail.com>
2025-07-29 16:30:45 -0700

    removed timing debug prints

--- model_funcs/ipsepc.cpp
+++ model_funcs/ipsepc.cpp
@@ -1,8 +1,5 @@
 #include "Cataclysmic_Variable.hh"
 
-#include <chrono>
-using namespace std::chrono;
-
 extern "C"
 void IPspec(const RealArray& energy, const RealArray& params, int spectrum_num, RealArray& flux, RealArray& err, const string& init_string)
 {
@@ -22,17 +19,9 @@ void IPspec(const RealArray& energy, const RealArray& params, int spectrum_num, 
 
     double mag_radius = r_m_ratio*cbrt(4*pi*pi*grav_const*mass*p_spin*p_spin);
 
-    auto start = high_resolution_clock::now();
     Cataclysmic_Variable intermediate_polar(mass, col_abund, luminosity, fractional_area, cos_incl, area_exponent, source_distance, reflection_sel, mag_radius);
-    std::cout << "Object Creation: " << duration_cast<milliseconds>(high_resolution_clock::now()-start).count() << " ms" << std::endl;
-    start = high_resolution_clock::now();
     intermediate_polar.Shock_Height_Shooting();
-    std::cout << "Solution Found:  " << duration_cast<milliseconds>(high_resolution_clock::now()-start).count() << " ms" << std::endl;
-    start = high_resolution_clock::now();
     intermediate_polar.Build_Column_Profile();
-    std::cout << "Profile Built:   " << duration_cast<milliseconds>(high_resolution_clock::now()-start).count() << " ms" << std::endl;
-    start = high_resolution_clock::now();
     intermediate_polar.MCVspec_Spectrum(energy, spectrum_num, flux, init_string);
-    std::cout << "Spectrum Produced:  " << duration_cast<milliseconds>(high_resolution_clock::now()-start).count() << " ms" << std::endl;
     intermediate_polar.Print_Properties();
 }
