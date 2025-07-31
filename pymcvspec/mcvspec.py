8bb5afcb85c601e0f8e6cd6fc263ad56361a4ac5
Gabriel Bridges <gabrielb71@gmail.com>
2025-07-31 16:44:28 -0700

    fixed computation of volume for pymcvspec

--- pymcvspec/mcvspec.py
+++ pymcvspec/mcvspec.py
@@ -54,7 +54,7 @@ class cataclysmic_variable:
         session = pyatomdb.spectrum.CIESession()
         session.set_response(energy_bins.to_value(u.keV), raw=True)
         flux = np.zeros(len(energy_bins)-1)/(u.s*u.keV)
-        volume = np.append(0*u.cm, np.diff(self.altitude))/2 + np.append(np.diff(self.altitude),0*u.cm)/2
+        volume = np.append(0*u.cm, np.abs(np.diff(self.altitude)))/2 + np.append(np.abs(np.diff(self.altitude)),0*u.cm)/2
         volume *= self.accretion_area*((1+self.altitude/self.radius)**self.n)
         for kT, n_e, n_i, vol in zip(self.electron_temperature, self.electron_density, self.ion_density, volume):
             flux += (session.return_spectrum(kT.to_value(u.keV))*(u.cm**5)/u.s/energy_bins.unit)*n_e*n_i*vol/(4*np.pi*self.distance**2)
