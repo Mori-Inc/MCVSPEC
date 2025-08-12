fixed units for spectrum in pybindings

pymcvspec/mcvspec.py
@@ -52,10 +52,10 @@ class cataclysmic_variable:
         self.electron_pressure = self.cpp_impl.get_electron_pressure()*u.dyne/u.cm**2
         self.cyclotron_ratio = self.cpp_impl.get_cyclotron_ratio()
     @u.quantity_input
-    def spectrum(self, energy_bins:u.Quantity[u.keV]) -> u.Quantity[1/u.s/u.keV]:
+    def spectrum(self, energy_bins:u.Quantity[u.keV]) -> u.Quantity[1/u.s/u.keV/u.cm**2]:
         session = pyatomdb.spectrum.CIESession()
         session.set_response(energy_bins.to_value(u.keV), raw=True)
-        flux = np.zeros(len(energy_bins)-1)/(u.s*u.keV)
+        flux = np.zeros(len(energy_bins)-1)/(u.s*u.keV*u.cm**2)
         volume = np.append(0*u.cm, np.abs(np.diff(self.altitude)))/2 + np.append(np.abs(np.diff(self.altitude)),0*u.cm)/2
         volume *= self.accretion_area*((1+self.altitude/self.radius)**self.n)
         for kT, n_e, n_i, vol in zip(self.electron_temperature, self.electron_density, self.ion_density, volume):
@@ -64,7 +64,7 @@ class cataclysmic_variable:
             # so I assign units here of emissivity instead
             # If an arf was set one would need to first divide by the arf to get back to the appropriate norm
             flux += (session.return_spectrum(kT.to_value(u.keV))*(u.cm**3)/u.s/energy_bins.unit)*n_e*n_i*vol/(4*np.pi*self.distance**2)
-        return flux.to(1/u.s/u.keV)
+        return flux.to(1/u.s/u.keV/u.cm**2)
 
 class polar(cataclysmic_variable):
     @u.quantity_input